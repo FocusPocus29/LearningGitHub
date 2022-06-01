ed0bbdc0efd5deb1554a0ca9f3f1a5637140c67c
John Papas <contacthome1@gmail.com>
2022-06-01 15:45:02 +0100

    Initial Commit

---
+++ LearningGitHub/LearningGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGitHubApp.swift
+//  LearningGitHub
+//
+//  Created by user213270 on 6/1/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
